01401980a5134244c1a003bead8ca75f7197fc4c
Ke Man Yi Nghi <ke.yinghi@student.vinci.be>
2022-12-16 15:50:37 +0100

    modify routes

--- src/Components/Router/Navigate.js
+++ src/Components/Router/Navigate.js
@@ -5,12 +5,13 @@
  * indirectly, the pages, then there would be a circular reference because the router
  * has to import all the pages to render them.
  */
+import { usePathPrefix } from '../../utils/path-prefix';
 
 const Navigate = (toUri) => {
   const fromUri = window.location.pathname;
   if (fromUri === toUri) return;
 
-  window.history.pushState({}, '', toUri);
+  window.history.pushState({}, '', usePathPrefix(toUri));
   const popStateEvent = new PopStateEvent('popstate', { state: {} });
   dispatchEvent(popStateEvent);
 };
--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -1,3 +1,4 @@
+import { removePathPrefix, usePathPrefix } from '../../utils/path-prefix';
 import HomePage from '../Pages/HomePage';
 import RegisterPage from '../Pages/RegisterPage';
 import LoginPage from '../Pages/LoginPage';
@@ -34,14 +35,14 @@ function onNavBarClick() {
       if (!componentToRender) throw Error(`The ${uri} ressource does not exist.`);
 
       componentToRender();
-      window.history.pushState({}, '', uri);
+      window.history.pushState({}, '', usePathPrefix(uri));
     }
   });
 }
 
 function onHistoryChange() {
   window.addEventListener('popstate', () => {
-    const uri = window.location.pathname;
+    const uri = removePathPrefix(window.location.pathname);
     const componentToRender = routes[uri];
     componentToRender();
   });
@@ -49,7 +50,7 @@ function onHistoryChange() {
 
 function onFrontendLoad() {
   window.addEventListener('load', () => {
-    const uri = window.location.pathname;
+    const uri = removePathPrefix(window.location.pathname);
     const componentToRender = routes[uri];
     if (!componentToRender) throw Error(`The ${uri} ressource does not exist.`);
 
